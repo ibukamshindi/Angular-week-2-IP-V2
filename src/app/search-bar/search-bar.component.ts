c8f0dd1cbac079279b311c1db137f2503f5965c4
ibukamshindi <ibukamshindi@gmail.com>
2020-08-30 16:16:22 +0300

    mported HttPClient, user class and repository class

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from '../user-class/user';
+import {Repository} from '../repository-class/repository';
 
 @Component({
   selector: 'app-search-bar',
