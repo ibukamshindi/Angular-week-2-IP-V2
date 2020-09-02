023bdddc48ed5306861a8bb529037d64ebbefb37
ibukamshindi <ibukamshindi@gmail.com>
2020-09-02 07:34:10 +0300

    add contentto about me and router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import{AboutMeComponent} from './about-me/about-me.component'
+import{AboutMeComponent} from './about-me/about-me.component';
 
 const routes: Routes = [
   {path:'aboutme',component:AboutMeComponent}
