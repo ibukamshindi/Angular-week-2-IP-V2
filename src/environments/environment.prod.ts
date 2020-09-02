3205af6ea024808c7c192d702d9e2b58aa0d0a98
ibukamshindi <ibukamshindi@gmail.com>
2020-09-02 18:49:18 +0300

    add apiUrl and gitApiKey to environment.production for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl :'https://api.github.com/users/',
+  gitApiKey:'?acess_token=03fec474368a1b5cd7c7e402f1f2ad0ac0d06fe0'
 };
